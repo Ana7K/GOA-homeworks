7a711f3349a9c8836fdcb1f612dfb75bb303cdb8
Ana7K <anakitesashvili4@gmail.com>
2025-03-02 11:18:06 +0400

    first commit

---
+++ day01/classwork/lesson01.py
@@ -0,0 +1,12 @@
+name = "ana"
+
+#print(name)
+
+age = 21
+height = 168.5
+
+print(type(name))
+print(type(age))
+print(type(height))
+
+print(name + " is " + str(age) + " years old")
---
+++ day01/homework/lesson01.py
@@ -0,0 +1,10 @@
+name = "Ana"
+age = 21
+height = 168.5
+
+print(type(name))
+print(type(age))
+print(type(height))
+
+print("My name is " + name + ". I'm " + str(age) + 
+    " years old. My height is about " + str(height) + " cm.")
